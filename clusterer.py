10f0fc9181873100c7c18673341e0f2690de0d8a
Nils Braun <nils.braun@kit.edu>
2017-03-07 23:31:26 +0100

    Use numpy arrays instead of pandas

--- clusterer.py
+++ clusterer.py
@@ -1,6 +1,14 @@
+from collections import defaultdict
+
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator
+
+layer_id = 0
+phi_id = 1
+x_id = 2
+y_id = 3
+number_id = 4
 
 
 class Clusterer(BaseEstimator):
@@ -15,65 +23,87 @@ class Clusterer(BaseEstimator):
         self.cut = cut
         self.duplicate_cut = duplicate_cut
 
+        self.layers = list(range(9))
+
+        self.hit_masks_grouped_by_layer = {}
+        self.not_used_mask = None
+
     def fit(self, X, y):
         pass
 
-    def get_weight(self, hit_1, hit_2, X_event, track):
+    @staticmethod
+    @np.vectorize
+    def get_weight(hit_1, hit_2):
         #return hit_1.cluster_id == hit_2.cluster_id
-        return abs(hit_1.phi - hit_2.phi) < 0.01
+        return abs(hit_1 - hit_2) < 0.01
 
     def get_quality(self, track):
         return 1
 
-    def walk(self, hit, X_event, track):
-        if hit.layer == 0:
+    def walk(self, hit, track, X_event):
+        # abort criteria
+        if hit[layer_id] == 0:
             yield track
-        else:
-            hits_on_next_layer = X_event[(X_event.layer == hit.layer - 1) & (X_event.not_used == True)]
+            return
 
-            if len(hits_on_next_layer) == 0:
-                yield track
-            else:
+        unused_hits_on_next_layer_mask = self.hit_masks_grouped_by_layer[hit[layer_id] - 1] & self.not_used_mask
 
-                hits_on_next_layer["weight"] = hits_on_next_layer.apply(lambda next_hit: self.get_weight(hit, next_hit, X_event, track), axis=1)
+        unused_hits_on_next_layer = X_event[unused_hits_on_next_layer_mask]
 
-                maximal_weight = hits_on_next_layer.weight.max()
+        if np.all(~unused_hits_on_next_layer_mask):
+            yield track
+            return
 
-                if maximal_weight < self.cut:
-                    yield track
-                # possible abort here
-                else:
-                    possible_next_hits = hits_on_next_layer[maximal_weight - hits_on_next_layer.weight < self.duplicate_cut]
+        weights = self.get_weight(hit[phi_id], unused_hits_on_next_layer[:, phi_id])
 
-                    for possible_next_hit_id in possible_next_hits.index:
-                        possible_next_hit = possible_next_hits.loc[possible_next_hit_id]
-                        for track_candidate in self.walk(possible_next_hit, X_event,
-                                                         track + [possible_next_hit]):
-                            yield track_candidate
+        maximal_weight = np.max(weights)
+
+        if maximal_weight < self.cut:
+            yield track
+            return
+
+        possible_next_hits_mask = maximal_weight - weights < self.duplicate_cut
+
+        for possible_next_hit in unused_hits_on_next_layer[possible_next_hits_mask]:
+            for track_candidate in self.walk(possible_next_hit,
+                                             track + [int(possible_next_hit[number_id])],
+                                             X_event,):
+                yield track_candidate
 
     def predict_single_event(self, X_event):
-        tmp = pd.DataFrame()
+        # Attention! We are redefining the iphi column here, as we do not need it
+        X_event[:, phi_id] = np.arctan2(X_event[:, y_id], X_event[:, x_id])
 
-        tmp["x"] = X_event[:, 2]
-        tmp["y"] = X_event[:, 3]
-        tmp["layer"] = X_event[:, 0]
-        tmp["phi"] = np.arctan2(tmp.y, tmp.x)
+        # Add a another column to store the hit ids
+        number_of_hits = len(X_event)
+        X_event = np.concatenate([X_event, np.reshape(np.arange(number_of_hits), (number_of_hits, 1))], axis=1)
 
-        tmp["not_used"] = True
-        tmp["label"] = -1
+        for layer in self.layers:
+            self.hit_masks_grouped_by_layer[layer] = X_event[:, layer_id] == layer
+
+        self.not_used_mask = np.ones(len(X_event)).astype("bool")
+        labels = -1 * np.ones(len(X_event))
 
         track_counter = 0
 
-        while tmp.not_used.sum() > 0:
-            start_hit = tmp[tmp.not_used].sort_values("layer", ascending=False).iloc[0]
-            track_list = self.walk(start_hit, tmp, [start_hit])
+        for layer in reversed(self.layers):
+            while True:
+                unused_mask_in_this_layer = self.hit_masks_grouped_by_layer[layer] & self.not_used_mask
 
-            best_track = max(track_list, key=lambda track: self.get_quality(track))
+                if not np.any(unused_mask_in_this_layer):
+                    break
 
-            for hit in best_track:
-                tmp.loc[hit.name, "label"] = track_counter
-                tmp.loc[hit.name, "not_used"] = False
-            track_counter += 1
+                start_hit = X_event[unused_mask_in_this_layer][0]
 
-        return tmp["label"].values
+                track_list = list(self.walk(start_hit, [int(start_hit[number_id])], X_event))
+
+                best_track = max(track_list, key=lambda track: self.get_quality(track))
+
+                # Store best track
+                self.not_used_mask[best_track] = False
+                labels[best_track] = track_counter
+
+                track_counter += 1
+
+        return labels
 
